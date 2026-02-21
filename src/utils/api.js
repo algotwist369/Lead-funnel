7d323af883fd5ba191020757ff25e7ca78c7e2c4
ankit pathak <info.algotwist@gmail.com>
2026-02-21 15:31:31 +0530

    updated with real backend api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:7001/api";
+const API_BASE_URL = `https://lead-funnel.api.ramaai.cloud/api` || "http://localhost:7001/api";
 
 export default API_BASE_URL;
 
